c104d9d434c882642c617111a9768aca1c500c63
arvirmdn <arviistecu@gmail.com>
2026-09-01 05:38:56 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: '20mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/upload', require('./routes/upload'));
